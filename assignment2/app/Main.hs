81d3b4ec2f8db164e5717c67c672351ab3a8618d
lbernath1 <leon.bernath@posteo.de>
2024-05-03 18:42:29 +0200

    added tests from discord

--- assignment2/app/Main.hs
+++ assignment2/app/Main.hs
@@ -1,6 +1,39 @@
-module Main (main) where
+-- Ellen Hedberg el1185he-s, Leon Bernáth le1327be-s
+-- 
 
+
+
+module Main where
 import Lib
+import Test.HUnit
+main =
+  runTestTT $
+    test
+      [ "maximaBy" ~: maximaByTest
+      , "similarityScore" ~: similarityScoreTest
+      , "optAliggments" ~: optAlignmentsTest
+      , "newSimilarityScore" ~: newSimilarityScoreTest
+      , "newOptAlignments" ~: newOptAlignmentsTest
+      , "mscLength" ~: mcsLengthTest
+      ]
 
-main :: IO ()
-main = someFunc
+todo = 42
+similarityScore string1 string2 = todo
+maximaBy valueFcn xs = xs -- todo
+optAlignments string1 string2 = [("todo", "todo")]
+outputOptAlignments string1 string2 = putStrLn ("TODO")
+newSimilarityScore string1 string2 = todo
+newOptAlignments string1 string2 = [("todo", "todo")]
+
+
+
+similarityScore :: String -> String -> Int
+similarityScore string1 string2
+
+
+
+score :: Char -> Int
+score(x,'-') = scoreSpace
+score('-',y) = scoreSpace
+score(x,y) = scoreMatch, if x == y
+             scoreMismatch, if x /= y
---
+++ assignment2/test/test.hs
@@ -0,0 +1,7 @@
+import Data.List
+similarityScoreTest = test [similarityScore "writers" "vintner" ~=? -5] 
+maximaByTest = test [sort (maximaBy length ["cs", "efd", "lth", "it"]) ~=? sort ["efd", "lth"]]
+optAlignmentsTest = test [sort (optAlignments "writers" "vintner") ~=? sort [("writ-ers", "vintner-"), ("wri-t-ers", "-vintner-"), ("wri-t-ers", "v-intner-")]]
+newSimilarityScoreTest = test [newSimilarityScore "writers" "vintner" ~=? -5]
+newOptAlignmentsTest = test [sort(newOptAlignments "writers" "vintner") ~=? sort [("writ-ers", "vintner-"), ("wri-t-ers", "-vintner-"), ("wri-t-ers", "v-intner-")]]
+mcsLengthTest = test [mcsLength [3, 2, 8, 2, 3, 9, 4, 3, 9] [1, 3, 2, 3, 7, 9] ~=? 4] -- [3, 2, 3, 9]
