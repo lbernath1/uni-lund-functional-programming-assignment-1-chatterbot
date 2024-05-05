3d9e675461512b5d74e20f5f9b26db75882dfe18
lbernath1 <leon.bernath@posteo.de>
2024-05-05 13:44:24 +0200

    wrote maximaBy function

--- assignment2/src/StringAlignments.hs
+++ assignment2/src/StringAlignments.hs
@@ -19,13 +19,13 @@ mcsLength xs ys = mcsLen (length xs) (length ys)
   where
     mcsLen i j = mcsTable!!i!!j
     mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
-       
+
     mcsEntry :: Int -> Int -> Int
     mcsEntry _ 0 = 0
     mcsEntry 0 _ = 0
     mcsEntry i j
       | x == y    = 1 + mcsLen (i-1) (j-1)
-      | otherwise = max (mcsLen i (j-1)) 
+      | otherwise = max (mcsLen i (j-1))
                         (mcsLen (i-1) j)
       where
          x = xs!!(i-1)
@@ -34,8 +34,10 @@ mcsLength xs ys = mcsLen (length xs) (length ys)
 
 type AlignmentType = (String,String)
 
-maximaBy :: Ord b => (a -> b) -> [a] -> [a] 
-maximaBy valueFcn xs = xs -- todo
+maximaBy :: Ord b => (a -> b) -> [a] -> [a]
+maximaBy valueFcn xs = filter (\ x -> valueFcn x >= a  ) xs
+  where
+    a = maximum (map valueFcn xs)
 
 
 
@@ -43,8 +45,12 @@ optAlignments :: String -> String -> [AlignmentType]
 
 similarityScore :: String -> String -> Int
 
-attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])] 
-attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList] 
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+
+
+
 
 todo = 42
 similarityScore string1 string2 = todo
@@ -57,11 +63,11 @@ newOptAlignments string1 string2 = [("todo", "todo")]
 
 
 
-similarityScoreTest = test [similarityScore "writers" "vintner" ~=? -5] 
+similarityScoreTest = test [similarityScore "writers" "vintner" ~=? -5]
 maximaByTest = test [sort (maximaBy length ["cs", "efd", "lth", "it"]) ~=? sort ["efd", "lth"]]
 optAlignmentsTest = test [sort (optAlignments "writers" "vintner") ~=? sort [("writ-ers", "vintner-"), ("wri-t-ers", "-vintner-"), ("wri-t-ers", "v-intner-")]]
 newSimilarityScoreTest = test [newSimilarityScore "writers" "vintner" ~=? -5]
-newOptAlignmentsTest = test [sort(newOptAlignments "writers" "vintner") ~=? sort [("writ-ers", "vintner-"), ("wri-t-ers", "-vintner-"), ("wri-t-ers", "v-intner-")]]
+newOptAlignmentsTest = test [sort (newOptAlignments "writers" "vintner") ~=? sort [("writ-ers", "vintner-"), ("wri-t-ers", "-vintner-"), ("wri-t-ers", "v-intner-")]]
 mcsLengthTest = test [mcsLength [3, 2, 8, 2, 3, 9, 4, 3, 9] [1, 3, 2, 3, 7, 9] ~=? 4] -- [3, 2, 3, 9]
 
 
