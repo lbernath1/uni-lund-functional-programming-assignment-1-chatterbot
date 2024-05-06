1c352eee59a2a28659b67b06602294811f448788
Ellen Hedberg <ellen.hedberg.1185@student.lu.se>
2024-05-06 20:03:29 +0200

    finished the lab

--- assignment2/src/StringAlignments.hs
+++ assignment2/src/StringAlignments.hs
@@ -36,9 +36,6 @@ mcsLength xs ys = mcsLen (length xs) (length ys)
          x = xs!!(i-1)
          y = ys!!(j-1)
 
-
-
-
 similarityScore :: String -> String -> Int
 similarityScore s1 s2 = simScores (length s1) (length s2)
   where
@@ -52,6 +49,23 @@ similarityScore s1 s2 = simScores (length s1) (length s2)
         simsEntry i (j-1) + scoreSpace,
         simsEntry (i-1) j + scoreSpace,
         if x == y then simsEntry (i-1) (j-1) + scoreMatch else simsEntry (i-1) (j-1) + scoreMismatch]
+      where
+         x = s1!!(i-1)
+         y = s2!!(j-1)
+
+newSimilarityScore :: String -> String -> Int
+newSimilarityScore s1 s2 = simScores (length s1) (length s2)
+  where
+    simScores i j = simScoreTable!!i!!j
+    simScoreTable = [[ simsEntry i j | j<-[0..]] | i<-[0..] ]
+
+    simsEntry :: Int -> Int -> Int
+    simsEntry a 0 = a*scoreSpace
+    simsEntry 0 a = a*scoreSpace
+    simsEntry i j = maximum [
+        simScores i (j-1) + scoreSpace,
+        simScores (i-1) j + scoreSpace,
+        if x == y then simScores (i-1) (j-1) + scoreMatch else simScores (i-1) (j-1) + scoreMismatch]
       where
          x = s1!!(i-1)
          y = s2!!(j-1)
@@ -78,13 +92,32 @@ optAlignments s1 s2 = reverseStrings $ snd $ optAligns (length s1) (length s2)
     optAlignTable = [[ oaEntry i j | j<-[0..]] | i<-[0..] ]
 
     oaEntry :: Int -> Int -> (Int, [AlignmentType])
-    oaEntry i 0 = (i*scoreSpace, [(['-'|z<-[1..i]], take i s2)])
-    oaEntry 0 j = (j*scoreSpace, [(take j s1, ['-'|z<-[1..j]])])
+    oaEntry 0 i = (i*scoreSpace, [(['-'|z<-[1..i]], take i s2)])
+    oaEntry j 0 = (j*scoreSpace, [(take j s1, ['-'|z<-[1..j]])])
 
     oaEntry i j = changeType  (maximaBy fst [
       (fst (oaEntry i (j-1)) + scoreSpace, attachHeads '-' y (snd $ oaEntry i (j-1))),
       (fst (oaEntry (i-1) j) + scoreSpace, attachHeads x '-' (snd $ oaEntry (i-1) j)),
       (if x == y then fst  (oaEntry (i-1) (j-1)) + scoreMatch else fst (oaEntry (i-1) (j-1)) + scoreMismatch, attachHeads x y (snd $ oaEntry (i-1) (j-1)))
+      ])
+      where
+         x = s1!!(i-1)
+         y = s2!!(j-1)
+
+newOptAlignments :: String -> String -> [AlignmentType]
+newOptAlignments s1 s2 = reverseStrings $ snd $ optAligns (length s1) (length s2)
+  where
+    optAligns i j = optAlignTable!!i!!j
+    optAlignTable = [[ oaEntry i j | j<-[0..]] | i<-[0..] ]
+
+    oaEntry :: Int -> Int -> (Int, [AlignmentType])
+    oaEntry 0 i = (i*scoreSpace, [(['-'|z<-[1..i]], take i s2)])
+    oaEntry j 0 = (j*scoreSpace, [(take j s1, ['-'|z<-[1..j]])])
+
+    oaEntry i j = changeType  (maximaBy fst [
+      (fst (optAligns i (j-1)) + scoreSpace, attachHeads '-' y (snd $ optAligns i (j-1))),
+      (fst (optAligns (i-1) j) + scoreSpace, attachHeads x '-' (snd $ optAligns (i-1) j)),
+      (if x == y then fst  (optAligns (i-1) (j-1)) + scoreMatch else fst (optAligns (i-1) (j-1)) + scoreMismatch, attachHeads x y (snd $ optAligns (i-1) (j-1)))
       ])
       where
          x = s1!!(i-1)
@@ -100,7 +133,11 @@ reverseStrings = map (Data.Bifunctor.bimap reverse reverse )
 
 
 outputOptAlignments :: String -> String -> IO()
-outputOptAlignments s1 s2 = printListofAlignments $ optAlignments s1 s2
+outputOptAlignments s1 s2 = do
+  putStrLn "number of opt aligns: "
+  print (length optAligns)
+  printListofAlignments optAligns
+  where optAligns = optAlignments s1 s2
 
 
 printListofAlignments :: [AlignmentType] -> IO()
@@ -113,10 +150,6 @@ printListofAlignments (tuple:listofTuples) = do
 
 string1 = "writers"
 string2 = "vintner"
-
-todo = 42
-newSimilarityScore string1 string2 = todo
-newOptAlignments string1 string2 = [("todo", "todo")]
 
 
 similarityScoreTest = test [similarityScore "writers" "vintner" ~=? -5]
