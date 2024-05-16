d5483e3fb449d6324a7c1623cb9d8faceda7b7a3
Leon Bernath <le1327be-s@varg-3.e.lth.lu.se>
2024-05-16 10:36:55 +0200

    partial-statement parsers

--- assignment3N/Statement.hs
+++ assignment3N/Statement.hs
@@ -6,13 +6,35 @@ import qualified Expr
 type T = Statement
 data Statement =
     Assignment String Expr.T 
-    | Skip | BeginEnds Statements | IfElse Expr.T Statement Statement | While Expr.T Statement | Read String | Write Expr.T | If Expr.T Statement Statement
+    | Skip | BeginEnds Statements | IfElse Expr.T Statement Statement | While Expr.T Statement | Read String | Write Expr.T 
     deriving Show
 
 type Statements = [Statement]
 
+
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
+
+skip = accept "skip" #- require ";" >-> buildSkip
+buildSkip a = Skip
+
+beginends = accept "begin" -# parseStatements #- require "ends" >-> buildBeginEnds
+buildBeginEnds s = BeginEnds s 
+
+ifelse = accept "if" -# Expr.parse #- require "then" # parseStatement #- require "else" # parseStatement >-> buildIfElse
+buildIfElse (e, s1, s2) = IfElse e s1 s2  
+
+whileparser = accept "while" -# Expr.parse #- require "do" # parseStatement >-> buildWhile
+buildWhile (e, s) = While e s
+
+readparser = accept "read" -# word #- require ";" >-> buildRead
+buildRead v = Read v
+
+writeParser = accept "write" -# Expr.parse #- require ";" >-> buildWrite
+buildWrite e = Write e 
+
+
+parseStatements = iter parseStatement
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
 exec (If cond thenStmts elseStmts: stmts) dict input = 
