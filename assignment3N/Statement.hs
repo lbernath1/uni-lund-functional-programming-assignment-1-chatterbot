dc3ee24e8b62e3343093ce206e427c57d813af55
Leon Bernath <le1327be-s@varg-3.e.lth.lu.se>
2024-05-16 11:24:32 +0200

    fixed statement.hs

--- assignment3N/Statement.hs
+++ assignment3N/Statement.hs
@@ -6,7 +6,7 @@ import qualified Expr
 type T = Statement
 data Statement =
     Assignment String Expr.T 
-    | Skip | BeginEnds Statements | IfElse Expr.T Statement Statement | While Expr.T Statement | Read String | Write Expr.T 
+    | Skip | BeginEnds Statements | If Expr.T Statement Statement | While Expr.T Statement | Read String | Write Expr.T 
     deriving Show
 
 type Statements = [Statement]
@@ -18,11 +18,11 @@ buildAss (v, e) = Assignment v e
 skip = accept "skip" #- require ";" >-> buildSkip
 buildSkip a = Skip
 
-beginends = accept "begin" -# parseStatements #- require "ends" >-> buildBeginEnds
+beginends = accept "begin" -# parseStatements #- require "end" >-> buildBeginEnds
 buildBeginEnds s = BeginEnds s 
 
-ifelse = accept "if" -# Expr.parse #- require "then" # parseStatement #- require "else" # parseStatement >-> buildIfElse
-buildIfElse (e, s1, s2) = IfElse e s1 s2  
+ifparser = accept "if" -# Expr.parse #- require "then" # parseStatement #- require "else" # parseStatement >-> buildIf
+buildIf ((e, s1), s2) = If e s1 s2  
 
 whileparser = accept "while" -# Expr.parse #- require "do" # parseStatement >-> buildWhile
 buildWhile (e, s) = While e s
@@ -36,7 +36,7 @@ buildWrite e = Write e
 
 parseStatements = iter parseStatement
 
-parseStatement = assignment ! skip ! beginends ! ifelse ! whileparser ! readparser ! writeParser
+parseStatement = assignment ! skip ! beginends ! ifparser ! whileparser ! readparser ! writeParser
 
 
 
@@ -47,7 +47,7 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     else exec (elseStmts: stmts) dict input
 
 instance Parse Statement where
-  parse = error "Statement.parse not implemented"
+  parse = parseStatement
   toString = error "Statement.toString not implemented"
 
 
