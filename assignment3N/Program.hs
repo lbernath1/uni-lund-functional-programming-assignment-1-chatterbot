94b856bd52ea44aab67fe43e830c14486ea495bf
Ellen Hedberg <ellen.hedberg.1185@student.lu.se>
2024-05-24 12:55:46 +0200

    made Program.hs compilable

--- assignment3N/Program.hs
+++ assignment3N/Program.hs
@@ -3,18 +3,18 @@ import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
+import Data.Maybe (Maybe(Nothing), fromJust, isJust, isNothing)
 newtype T = Program Statement.Statements
 
 
 programToStatements :: T -> Statement.Statements
 programToStatements (Program program) = program  
 
-tupleStatementToProgram :: Maybe (Statement.Statements, String ) -> Maybe (Statement.Statements, String)
-tupleStatementToProgram (a, b) = (Program a, b)
+instance Parse T where --parse takes string, returns (program, string), toString takes program, returns string
+  parse s = Just ((Program stmts), "")
+    where stmts = fst (fromJust (Statement.parseStatements s))
 
-instance Parse T where
-  parse program = tupleStatementToProgram $ Statement.parseStatements (programToStatements program) 
-  toString program = Statement.statementsToString (programToStatements program) 0
+  toString (Program program) = Statement.statementsToString program 0
 
 
 exec program input = Statement.exec program dict input  --make point free-er later
