e222d0c723934639ce50fc558f9573bc3a890515
Leon Bernath <le1327be-s@lo-1.e.lth.lu.se>
2024-05-24 13:04:52 +0200

    added correct parse and toString functions in program.hs

--- assignment3N/Program.hs
+++ assignment3N/Program.hs
@@ -7,15 +7,11 @@ import Data.Maybe (Maybe(Nothing), fromJust, isJust, isNothing)
 newtype T = Program Statement.Statements
 
 
-programToStatements :: T -> Statement.Statements
-programToStatements (Program program) = program  
 
-instance Parse T where --parse takes string, returns (program, string), toString takes program, returns string
-  parse s = Just ((Program stmts), "")
-    where stmts = fst (fromJust (Statement.parseStatements s))
-
+instance Parse T where 
+  parse = Statement.parseStatements >-> Program
   toString (Program program) = Statement.statementsToString program 0
 
 
-exec program input = Statement.exec program dict input  --make point free-er later
+exec (Program program) input = Statement.exec program dict input  --make point free-er later
   where dict = Dictionary.empty
