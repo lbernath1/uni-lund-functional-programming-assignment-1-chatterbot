0a336bf1bfa34d7afaa4fbde923d002c009f48b2
Ellen Hedberg <ellen.hedberg.1185@student.lu.se>
2024-07-03 14:44:50 +0200

    redid exec prime function so it works

--- assignment3N/Statement.hs
+++ assignment3N/Statement.hs
@@ -1,8 +1,5 @@
 
 --Ellen Hedberg & Leon Bernáth
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant bracket" #-}
-{-# HLINT ignore "Eta reduce" #-}
 module Statement(T, parse, toString, fromString, exec, Statement, Statements, parseStatements, statementToString, statementsToString) where
 import Prelude hiding (return, fail)
 import Parser hiding (T)
@@ -10,7 +7,7 @@ import qualified Dictionary
 import qualified Expr
 type T = Statement
 data Statement =
-    Assignment String Expr.T| Skip | BeginEnds Statements | If Expr.T Statement Statement | While Expr.T Statement | Read String | Write Expr.T
+    Assignment String Expr.T| Skip | BeginEnds Statements | If Expr.T Statement Statement | While Expr.T Statement | Read String | Write Expr.T 
     deriving Show
 
 type Statements = [Statement]
@@ -22,10 +19,10 @@ skip = accept "skip" #- require ";" >-> buildSkip
 buildSkip a = Skip
 
 beginends = accept "begin" -# parseStatements #- require "end" >-> buildBeginEnds
-buildBeginEnds s = BeginEnds s
+buildBeginEnds s = BeginEnds s 
 
 ifparser = accept "if" -# Expr.parse #- require "then" # parseStatement #- require "else" # parseStatement >-> buildIf
-buildIf ((e, s1), s2) = If e s1 s2
+buildIf ((e, s1), s2) = If e s1 s2  
 
 whileparser = accept "while" -# Expr.parse #- require "do" # parseStatement >-> buildWhile
 buildWhile (e, s) = While e s
@@ -34,7 +31,7 @@ readparser = accept "read" -# word #- require ";" >-> buildRead
 buildRead v = Read v
 
 writeParser = accept "write" -# Expr.parse #- require ";" >-> buildWrite
-buildWrite e = Write e
+buildWrite e = Write e 
 
 
 --commentParser = spaces -# accept "--" -# iter notnewline -# lit '\n' >-> buildSkip
@@ -43,61 +40,60 @@ parseStatement = assignment ! skip ! beginends ! ifparser ! whileparser ! readpa
 parseStatements = iter parseStatement
 
 
-first :: (a, b, c) -> a
-first (x, _, _) = x
+first :: (a, b, c, d) -> a
+first (w, _, _, _) = w 
 
-second :: (a, b, c) -> b
-second (_, y, _) = y
+second :: (a, b, c, d) -> b
+second (_, x, _, _) = x
 
-third :: (a, b, c) -> c
-third (_, _, z) = z
+third :: (a, b, c, d) -> c
+third (_, _, y, _) = y
+
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, z) = z 
 
 
-exec' :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer] -> (Dictionary.T String Integer, [Integer], [Integer])
-exec' [] dict input output = (dict, input, output)
+exec' :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer] -> ([T], Dictionary.T String Integer, [Integer], [Integer])
+exec' [] dict input output = ([], dict, input, output)
 exec' (Assignment varname valueExpr : stmts) dict input output = exec' stmts (Dictionary.insert (varname, Expr.value valueExpr dict) dict) input output
-exec' (Skip:stmnts) dict input output = exec' stmnts dict input output
-exec' (If cond thenStmts elseStmts : stmts) dict input output =
-    if Expr.value cond dict>0
+exec' (Skip:stmts) dict input output = exec' stmts dict input output
+exec' (If cond thenStmts elseStmts : stmts) dict input output = 
+    if (Expr.value cond dict)>0 
     then exec' (thenStmts: stmts) dict input output
     else exec' (elseStmts: stmts) dict input output
+exec' ((BeginEnds listofStatements) : stmts) dict input output = continuationOfProgram
+    where innerProgram = exec' listofStatements dict input output
+          continuationOfProgram = exec' stmts (second innerProgram)  (third innerProgram) (fourth innerProgram)
 
-exec' ((BeginEnds listofStatements) : stmts) dict input output = (first b, second b,  (third a) ++ (third b))
-    where a = exec' listofStatements dict input output
-          b = exec' stmts (first a)  (second a) []
-
-exec' (While cond statement : stmts) dict input output =
+exec' ((While cond statement) : stmts) dict input output =
     if (Expr.value cond dict)>0
-    then exec' (While cond statement : stmts) (first doWhileOnce) (second doWhileOnce) (output ++ (reverse (third doWhileOnce)))
+    then exec' ((While cond statement) : stmts) (second doWhileIteration) (third doWhileIteration) (fourth doWhileIteration)
     else exec' stmts dict input output
-       where doWhileOnce = exec' [statement] dict input []
-             
-      
+       where doWhileIteration = exec' [statement] dict input output
+
 exec' (Read varname : stmts) dict (val:input) output = exec' stmts (Dictionary.insert (varname, val) dict) input output
 
-exec' (Write expr : stmnts) dict input output = (first a, second a, third a ++[(Expr.value expr dict)] )
-    where a = exec' stmnts dict input output
-
+exec' (Write expr : stmts) dict input output = exec' stmts dict input (output ++ [Expr.value expr dict])
 
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-exec stmts dict input = third  $ exec' stmts dict input []
+exec stmts dict input = fourth  $ exec' stmts dict input []
 
 
 indentString = "  "
 
 indentf :: Int -> String -> String
 indentf 0 s = s
-indentf n s = indentf (n-1) (indentString++s)
+indentf n s = indentf (n-1) (indentString++s) 
 
 
 statementToString :: Statement -> Int -> String
-statementToString statement n = case statement of
+statementToString statement n = case statement of 
     Assignment var expr -> indentf n $ var ++ ":=" ++ (Expr.toString expr) ++ ";"  ++ "\n"
     Skip -> indentf n $ "skip;" ++ "\n"
     BeginEnds stmnts -> (indentf n "begin\n") ++ statementsToString stmnts (n+1)  ++ (indentf n "end\n")
-    If  expr sm1 sm2 -> (indentf n $ "if " ++ (Expr.toString expr) ++ " then\n") ++ statementToString sm1 (n+1)  ++ (indentf n "else\n") ++ statementToString sm2 (n+1)
-    While expr sm -> (indentf n $ "while " ++ (Expr.toString expr) ++ " do\n") ++ statementToString sm (n+1)
+    If  expr sm1 sm2 -> (indentf n $ "if " ++ (Expr.toString expr) ++ " then\n") ++ statementToString sm1 (n+1)  ++ (indentf n "else\n") ++ statementToString sm2 (n+1) 
+    While expr sm -> (indentf n $ "while " ++ (Expr.toString expr) ++ " do\n") ++ statementToString sm (n+1) 
     Read var -> indentf n $ "read "++ var ++";\n"
     Write expr -> indentf n $ "write "++  (Expr.toString expr) ++";\n"
 
